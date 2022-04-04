a8d14ecef94440d482623faac7b243a5c0aa563d
dimitarquanterall <d.aleksandrov@sap.com>
2022-04-04 17:37:33 +0300

    .xsaccess without name

--- ide-workspace/workspace.js
+++ ide-workspace/workspace.js
@@ -1136,7 +1136,6 @@ angular.module('workspace', ['workspace.config', 'ideUiCore', 'ngAnimate', 'ngSa
                                         fileNode.text = (fileTemplate.extension != 'xsaccess' ? 'file.' : '.') + fileTemplate.extension;
                                         fileNode.data = fileTemplate.data;
                                         tree.create_node(parentNode, fileNode, "last", function (new_node) {
-                                            console.log("NEW", new_node)
                                             if (new_node.text != '.xsaccess') {
                                                 tree.edit(new_node);
                                             }
